8f43c43c066825d0b40397da2d41429321f1e699
Partha-sinha <schrodinger.zerf@gmail.com>
2018-08-31 14:18:25 +0530

    Deep Learning - Chatbot

---
+++ Chatbot.py
@@ -0,0 +1,14 @@
+# Building a Chatbot
+
+# Importing the libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+## Data Preprocessing 
+
+# Importing the  dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
