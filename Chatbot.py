d897248f84f758dd269989f8cee38b8b491cc268
Partha-sinha <schrodinger.zerf@gmail.com>
2018-09-03 19:26:24 +0530

    Deep Learning - Chatbot

--- Chatbot.py
+++ Chatbot.py
@@ -18,4 +18,10 @@ for line in lines:
     _line = line.split(' +++$+++ ')
     if len(_line) == 5:
         id2line[_line[0]] = _line[4]
+
+# Creating a list of conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversations_ids.append(_conversation.split(','))
         
